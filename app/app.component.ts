00610768790171a03d30817b3b91f797e8646362
Josh Linton and Mark Fisher <mark@>
2017-05-01 15:22:15 -0700

    make some progress on two-way binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,11 +6,22 @@ import { Component } from '@angular/core';
   <div class="container">
     <div class="jumbotron">
       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    </div>
       <h3>{{currentFocus}}</h3>
       <ul>
-        <li *ngFor = "let currentTask of tasks">{{currentTask.description}}</li>
+        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}
+        <button (click)="editTask()">Edit!</button>
+        </li>
       </ul>
-    </div>
+      <hr>
+      <div>
+        <h3>{{selectedTask.description}}</h3>
+        <p>Task Complete?{{selectedTask.done}}</p>
+        <h3>Edit Task</h3>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+
+      </div>
   </div>
   `
 })
@@ -22,13 +33,28 @@ export class AppComponent{
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] =[
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 3),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+  selectedTask: Task = this.tasks[0];
+
+  priorityColor(currentTask){
+    if(currentTask.priority === 3){
+      return "bg-danger";
+    } else if(currentTask.priority === 2){
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  editTask(){
+    alert("You just requested to edit a Task!");
+  }
 }
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string) {}
+  constructor(public description: string, public priority: number) {}
 }
